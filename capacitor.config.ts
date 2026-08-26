b2e6026061d385e449c65ef01d0b5150a5f54ecb
ALZEE23 <muhamadalfarizial726@gmail.com>
2026-08-26 16:28:35 +0700

    feat: add mobile config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.myapp',
   appName: 'my-app',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
